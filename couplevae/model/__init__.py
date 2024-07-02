f81318753e29eb95bae6f43ada571812f3903967
LiminLi-xjtu <59948715+LiminLi-xjtu@users.noreply.github.com>
2024-07-03 03:16:19 +0800

    Update __init__.py

--- couplevae/model/__init__.py
+++ couplevae/model/__init__.py
@@ -1,3 +1,4 @@
 from ._couplevae import VAE
-from .util import train_test_split, data_remover, balancer
+from ._trainer import Trainer
+from .util import train_test_split, data_remover, balancer, load_h5ad_to_dataloader
 
